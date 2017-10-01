a5a9329d8fb3b1cb1207f754b983c76bdf801b44
Raistlin49 <raistlin492008@gmail.com>
2017-10-01 03:24:54 -0400

    MAX CHANGESET d833ab36ed5bee595b5daeb77a6e67e23ffb0e87

---
+++ Database/Release/SQL/Master/20171010/20171010_master_VDBMP_04_Rollback.sql
@@ -0,0 +1,36 @@
+--================================================================================================
+-- Description: SQL Release Script
+-- Created by:  Data Services
+-- Machine: TGC-LT-94JMM12
+-- Created on:  10-01-2017 03:24:33
+-- Path: /Database/Release/SQL/Patch/20171010/VDBMP
+-- Filter: Rollback
+--================================================================================================
+-- Manifest --
+--   DS-0000.01.Alter.Rollback.sql
+-- Manifest --
+
+--================================================================================================
+-- /Database/Release/SQL/Patch/20171010/VDBMP/DS-0000.01.Alter.Rollback.sql
+PRINT 'Processing DS-0000.01.Alter.Rollback.sql ******** Start'
+GO
+SET ANSI_NULLS ON;
+GO
+SET ANSI_PADDING ON;
+GO
+SET QUOTED_IDENTIFIER ON;
+GO
+PRINT 'Testing rollback...'
+;
+GO
+PRINT 'Processing DS-0000.01.Alter.Rollback.sql ******** End'
+GO
+--================================================================================================
+SET ANSI_NULLS ON;
+GO
+SET ANSI_PADDING ON;
+GO
+SET QUOTED_IDENTIFIER ON;
+GO
+--================================================================================================
+-- End of Script
