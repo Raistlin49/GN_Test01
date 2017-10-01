a5e3f0abcfdd0653d65d7e067947174ce5ba48ff
Raistlin49 <raistlin492008@gmail.com>
2017-10-01 02:13:30 -0400

    MAX CHANGESET 330e42d158af7001301ee822ed563a9983544007

---
+++ Database/Release/SQL/Master/20170912k/20170912k_master_VDBMP_04_Rollback.sql
@@ -0,0 +1,74 @@
+--================================================================================================
+-- Description: SQL Release Script
+-- Created by:  Data Services
+-- Machine: TGC-LT-94JMM12
+-- Created on:  10-01-2017 02:13:00
+-- Path: /Database/Release/SQL/Patch/20170912k/VDBMP
+-- Filter: Rollback
+--================================================================================================
+-- Manifest --
+--   000001.ROLLBACK.sql
+--   000002.ROLLBACK.sql
+--   000003.ROLLBACK.sql
+-- Manifest --
+
+--================================================================================================
+-- /Database/Release/SQL/Patch/20170912k/VDBMP/000001.ROLLBACK.sql
+PRINT 'Processing 000001.ROLLBACK.sql ******** Start'
+GO
+SET ANSI_NULLS ON;
+GO
+SET ANSI_PADDING ON;
+GO
+SET QUOTED_IDENTIFIER ON;
+GO
+SELECT 'ASDF';
+SELECT 'asdf';
+;
+GO
+PRINT 'Processing 000001.ROLLBACK.sql ******** End'
+GO
+--================================================================================================
+-- /Database/Release/SQL/Patch/20170912k/VDBMP/000002.ROLLBACK.sql
+PRINT 'Processing 000002.ROLLBACK.sql ******** Start'
+GO
+SET ANSI_NULLS ON;
+GO
+SET ANSI_PADDING ON;
+GO
+SET QUOTED_IDENTIFIER ON;
+GO
+--First post to GitHub
+--new line here
+--third line
+--fourth line
+--fifth line
+
+;
+GO
+PRINT 'Processing 000002.ROLLBACK.sql ******** End'
+GO
+--================================================================================================
+-- /Database/Release/SQL/Patch/20170912k/VDBMP/000003.ROLLBACK.sql
+PRINT 'Processing 000003.ROLLBACK.sql ******** Start'
+GO
+SET ANSI_NULLS ON;
+GO
+SET ANSI_PADDING ON;
+GO
+SET QUOTED_IDENTIFIER ON;
+GO
+--Second post to GitHub
+;
+GO
+PRINT 'Processing 000003.ROLLBACK.sql ******** End'
+GO
+--================================================================================================
+SET ANSI_NULLS ON;
+GO
+SET ANSI_PADDING ON;
+GO
+SET QUOTED_IDENTIFIER ON;
+GO
+--================================================================================================
+-- End of Script
