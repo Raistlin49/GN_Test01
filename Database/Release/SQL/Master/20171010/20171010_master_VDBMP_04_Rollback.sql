1e809f201704cf94bdd63b8bb8e35988922fcc16
Raistlin49 <raistlin492008@gmail.com>
2017-10-02 18:32:20 -0400

    MAX CHANGESET 8994b304421884152ee891beeb12de602c98db4b

--- Database/Release/SQL/Master/20171010/20171010_master_VDBMP_04_Rollback.sql
+++ Database/Release/SQL/Master/20171010/20171010_master_VDBMP_04_Rollback.sql
@@ -2,7 +2,7 @@
 -- Description: SQL Release Script
 -- Created by:  Data Services
 -- Machine: TGC-LT-94JMM12
--- Created on:  10-02-2017 16:51:59
+-- Created on:  10-02-2017 18:32:04
 -- Path: /Database/Release/SQL/Patch/20171010/VDBMP
 -- Filter: Rollback
 --================================================================================================
@@ -10,6 +10,7 @@
 --   DS-0000.01.Alter.Rollback.sql
 --   DS-0000.02.Alter.Rollback.sql
 --   DS-0000.03.Alter.Rollback.sql
+--   DS-0000.04.Alter.Rollback.sql
 -- Manifest --
 
 --================================================================================================
@@ -56,6 +57,21 @@ PRINT 'Testing rollback...'
 ;
 GO
 PRINT 'Processing DS-0000.03.Alter.Rollback.sql ******** End'
+GO
+--================================================================================================
+-- /Database/Release/SQL/Patch/20171010/VDBMP/DS-0000.04.Alter.Rollback.sql
+PRINT 'Processing DS-0000.04.Alter.Rollback.sql ******** Start'
+GO
+SET ANSI_NULLS ON;
+GO
+SET ANSI_PADDING ON;
+GO
+SET QUOTED_IDENTIFIER ON;
+GO
+PRINT 'Testing new 04 rollback...'
+;
+GO
+PRINT 'Processing DS-0000.04.Alter.Rollback.sql ******** End'
 GO
 --================================================================================================
 SET ANSI_NULLS ON;
