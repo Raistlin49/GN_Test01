d485ed759a1fbd9dd19e43d3376dea43ad6f9a13
Raistlin49 <raistlin492008@gmail.com>
2017-10-03 12:27:41 -0400

    MAX CHANGESET cf9f5819ec018af585b88c421e25ca628241ce85

---
+++ Database/Release/SQL/Master/20171012/20171012_master_VDBMP_02_Apply.sql
@@ -0,0 +1,63 @@
+--================================================================================================
+-- Description: SQL Release Script
+-- Created by:  Data Services
+-- Machine: TGC-LT-94JMM12
+-- Created on:  10-03-2017 12:27:33
+-- Path: /Database/Release/SQL/Patch/20171012/VDBMP
+-- Filter: Apply
+--================================================================================================
+-- Manifest --
+--   DS-0010.01.Alter.Apply.sql
+-- Manifest --
+
+--================================================================================================
+-- 
+PRINT 'Processing DS-0010.01.Alter.Apply.sql ******** Start'
+GO
+SET ANSI_NULLS ON;
+GO
+SET ANSI_PADDING ON;
+GO
+SET QUOTED_IDENTIFIER ON;
+GO
+USE CRM;
+GO
+
+PRINT 'This is some test code for DS-0010.01.Alter.Apply';
+GO
+;
+GO
+PRINT 'Processing DS-0010.01.Alter.Apply.sql ******** End'
+GO
+--================================================================================================
+SET ANSI_NULLS ON;
+GO
+SET ANSI_PADDING ON;
+GO
+SET QUOTED_IDENTIFIER ON;
+GO
+--================================================================================================
+-- End of Script
+
+USE [CRM]
+GO
+
+--Create missing table
+IF OBJECT_ID('[dbo].[dbverinfo]', 'U') IS NULL
+BEGIN
+	CREATE TABLE [dbo].[dbverinfo](
+		[BuildConfigId] [INT] NOT NULL,
+		[BuildKey] [VARCHAR](100) NOT NULL
+	) ON [PRIMARY]
+END;
+
+IF EXISTS (SELECT 1 FROM [dbo].[dbverinfo] WHERE [BuildConfigId] = 12)
+	BEGIN
+		UPDATE [dbo].[dbverinfo] SET [BuildKey] = 'e3571471-ea85-4d23-a5dc-0317df3bdfe0' WHERE [BuildConfigId] = 12
+	END
+ELSE
+	BEGIN
+		INSERT INTO [dbo].[dbverinfo]
+				( [BuildConfigId], [BuildKey] )
+		VALUES  ( 12, 'e3571471-ea85-4d23-a5dc-0317df3bdfe0')
+	END;
