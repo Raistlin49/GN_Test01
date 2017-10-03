204e0ae68714e13e37e6c483d37fbeac297e988a
Raistlin49 <raistlin492008@gmail.com>
2017-10-03 17:02:33 -0400

    MAX CHANGESET b87f5ac54e41a61002a20a79a2db7d8133cef637

--- Database/Release/SQL/Master/20171012/20171012_master_VDBMP_02_Apply.sql
+++ Database/Release/SQL/Master/20171012/20171012_master_VDBMP_02_Apply.sql
@@ -2,13 +2,14 @@
 -- Description: SQL Release Script
 -- Created by:  Data Services
 -- Machine: TGC-LT-94JMM12
--- Created on:  10-03-2017 16:45:09
+-- Created on:  10-03-2017 17:02:19
 -- Path: /Database/Release/SQL/Patch/20171012/VDBMP
 -- Filter: Apply
 --================================================================================================
 -- Manifest --
 --   DS-0010.01.Alter.Apply.sql
 --   DS-0010.02.Alter.Apply.sql
+--   DS-0010.03.Alter.Apply.sql
 -- Manifest --
 
 --================================================================================================
@@ -48,6 +49,25 @@ GO
 ;
 GO
 PRINT 'Processing DS-0010.02.Alter.Apply.sql ******** End'
+GO
+--================================================================================================
+-- /Database/Release/SQL/Patch/20171012/VDBMP/DS-0010.03.Alter.Apply.sql
+PRINT 'Processing DS-0010.03.Alter.Apply.sql ******** Start'
+GO
+SET ANSI_NULLS ON;
+GO
+SET ANSI_PADDING ON;
+GO
+SET QUOTED_IDENTIFIER ON;
+GO
+USE CRM;
+GO
+
+PRINT 'This is some test code for DS-0010.03.Alter.Apply';
+GO
+;
+GO
+PRINT 'Processing DS-0010.03.Alter.Apply.sql ******** End'
 GO
 --================================================================================================
 SET ANSI_NULLS ON;
