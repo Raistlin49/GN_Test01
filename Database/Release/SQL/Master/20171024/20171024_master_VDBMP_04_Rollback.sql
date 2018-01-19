9e92ecdf04d2d65397c6bee219e93a73c6b6429c
Raistlin49 <raistlin492008@gmail.com>
2018-01-18 19:02:41 -0500

    MAX CHANGESET 867e35e2bdf4da3436730c186837fe9e368f1d2f

--- Database/Release/SQL/Master/20171024/20171024_master_VDBMP_04_Rollback.sql
+++ Database/Release/SQL/Master/20171024/20171024_master_VDBMP_04_Rollback.sql
@@ -2,12 +2,13 @@
 -- Description: SQL Release Script
 -- Created by:  Data Services
 -- Machine: TGC-LT-94JMM12
--- Created on:  10-15-2017 03:41:57
+-- Created on:  01-18-2018 19:02:29
 -- Path: /Database/Release/SQL/Patch/20171024/VDBMP
 -- Filter: Rollback
 --================================================================================================
 -- Manifest --
 --   DS-1024.01.Alter.Rollback.sql
+--   DS-1024.02.Alter.Rollback.sql
 --   DS-1024.02.Alter.Rollback.sql
 -- Manifest --
 
@@ -26,6 +27,22 @@ PRINT 'The 3:22 AM line';
 ;
 GO
 PRINT 'Processing DS-1024.01.Alter.Rollback.sql ******** End'
+GO
+--================================================================================================
+-- /Database/Release/SQL/Patch/20171024/VDBMP/DS-1024.02.Alter.Rollback.sql
+PRINT 'Processing DS-1024.02.Alter.Rollback.sql ******** Start'
+GO
+SET ANSI_NULLS ON;
+GO
+SET ANSI_PADDING ON;
+GO
+SET QUOTED_IDENTIFIER ON;
+GO
+PRINT '02 - Testing code in the rollback script...'
+PRINT '3:24 AM';
+;
+GO
+PRINT 'Processing DS-1024.02.Alter.Rollback.sql ******** End'
 GO
 --================================================================================================
 -- /Database/Release/SQL/Patch/20171024/VDBMP/DS-1024.02.Alter.Rollback.sql
