b2d5da6682a95bc7cec88fb03dea604148401fc6
Raistlin49 <raistlin492008@gmail.com>
2018-01-18 21:49:52 -0500

    MAX CHANGESET 51a39da266dea2da90b8c119be1eb651887b40f6

---
+++ Database/Release/SQL/Master/20180107/20180107_master_VDBMP_02_Apply.sql
@@ -0,0 +1,66 @@
+--================================================================================================
+-- Description: SQL Release Script
+-- Created by:  Data Services
+-- Machine: TGC-LT-94JMM12
+-- Created on:  01-18-2018 21:49:46
+-- Path: /Database/Release/SQL/Patch/20180107/VDBMP
+-- Filter: Apply
+--================================================================================================
+-- Manifest --
+--   DS-1100.01.Create.Apply.sql
+-- Manifest --
+
+--================================================================================================
+-- /Database/Release/SQL/Patch/20180107/VDBMP/DS-1100.01.Create.Apply.sql
+PRINT 'Processing DS-1100.01.Create.Apply.sql ******** Start'
+GO
+SET ANSI_NULLS ON;
+GO
+SET ANSI_PADDING ON;
+GO
+SET QUOTED_IDENTIFIER ON;
+GO
+USE CRM;
+GO
+
+SELECT 'Happy New Year - Apply';
+GO
+
+SELECT GETDATE();
+GO
+;
+GO
+PRINT 'Processing DS-1100.01.Create.Apply.sql ******** End'
+GO
+--================================================================================================
+SET ANSI_NULLS ON;
+GO
+SET ANSI_PADDING ON;
+GO
+SET QUOTED_IDENTIFIER ON;
+GO
+--================================================================================================
+-- End of Script
+
+USE [CRM]
+GO
+
+--Create missing table
+IF OBJECT_ID('[dbo].[dbverinfo]', 'U') IS NULL
+BEGIN
+	CREATE TABLE [dbo].[dbverinfo](
+		[BuildConfigId] [INT] NOT NULL,
+		[BuildKey] [VARCHAR](100) NOT NULL
+	) ON [PRIMARY]
+END;
+
+IF EXISTS (SELECT 1 FROM [dbo].[dbverinfo] WHERE [BuildConfigId] = 13)
+	BEGIN
+		UPDATE [dbo].[dbverinfo] SET [BuildKey] = '1a3e3a66-85b0-4c26-bc28-269668d37956' WHERE [BuildConfigId] = 13
+	END
+ELSE
+	BEGIN
+		INSERT INTO [dbo].[dbverinfo]
+				( [BuildConfigId], [BuildKey] )
+		VALUES  ( 13, '1a3e3a66-85b0-4c26-bc28-269668d37956')
+	END;
