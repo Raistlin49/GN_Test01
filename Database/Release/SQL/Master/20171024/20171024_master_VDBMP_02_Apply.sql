042538cb158e3b4adefee491af4b25e7c890657f
Raistlin49 <raistlin492008@gmail.com>
2018-01-18 19:10:28 -0500

    MAX CHANGESET d60fbbf5b960f1192b8e8c7e4c486bd3b8f8418d

--- Database/Release/SQL/Master/20171024/20171024_master_VDBMP_02_Apply.sql
+++ Database/Release/SQL/Master/20171024/20171024_master_VDBMP_02_Apply.sql
@@ -2,12 +2,14 @@
 -- Description: SQL Release Script
 -- Created by:  Data Services
 -- Machine: TGC-LT-94JMM12
--- Created on:  01-18-2018 19:02:29
+-- Created on:  01-18-2018 19:10:19
 -- Path: /Database/Release/SQL/Patch/20171024/VDBMP
 -- Filter: Apply
 --================================================================================================
 -- Manifest --
 --   DS-1024.01.Alter.Apply.sql
+--   DS-1024.01.Alter.Apply.sql
+--   DS-1024.02.Alter.Apply.sql
 --   DS-1024.02.Alter.Apply.sql
 -- Manifest --
 
@@ -26,10 +28,50 @@ PRINT 'The 2:02 AM line';
 PRINT 'The 2:12 AM line';
 PRINT 'The 3:22 AM line';
 SELECT * FROM sys.tables;
+PRINT '2018-01-18 19:09';
 
 ;
 GO
 PRINT 'Processing DS-1024.01.Alter.Apply.sql ******** End'
+GO
+--================================================================================================
+-- /Database/Release/SQL/Patch/20171024/VDBMP/DS-1024.01.Alter.Apply.sql
+PRINT 'Processing DS-1024.01.Alter.Apply.sql ******** Start'
+GO
+SET ANSI_NULLS ON;
+GO
+SET ANSI_PADDING ON;
+GO
+SET QUOTED_IDENTIFIER ON;
+GO
+PRINT 'Testing code in the apply script...';
+PRINT 'The 2:02 AM line';
+PRINT 'The 2:12 AM line';
+PRINT 'The 3:22 AM line';
+SELECT * FROM sys.tables;
+PRINT '2018-01-18 19:09';
+
+;
+GO
+PRINT 'Processing DS-1024.01.Alter.Apply.sql ******** End'
+GO
+--================================================================================================
+-- /Database/Release/SQL/Patch/20171024/VDBMP/DS-1024.02.Alter.Apply.sql
+PRINT 'Processing DS-1024.02.Alter.Apply.sql ******** Start'
+GO
+SET ANSI_NULLS ON;
+GO
+SET ANSI_PADDING ON;
+GO
+SET QUOTED_IDENTIFIER ON;
+GO
+PRINT '02 - Testing code in the apply script...';
+PRINT 'The 3:18 AM line';
+PRINT '';
+PRINT '3:24 AM';
+;
+GO
+PRINT 'Processing DS-1024.02.Alter.Apply.sql ******** End'
 GO
 --================================================================================================
 -- /Database/Release/SQL/Patch/20171024/VDBMP/DS-1024.02.Alter.Apply.sql
@@ -73,11 +115,11 @@ END;
 
 IF EXISTS (SELECT 1 FROM [dbo].[dbverinfo] WHERE [BuildConfigId] = 13)
 	BEGIN
-		UPDATE [dbo].[dbverinfo] SET [BuildKey] = '1cff21d4-c440-4e1f-b075-427ad0d7ff74' WHERE [BuildConfigId] = 13
+		UPDATE [dbo].[dbverinfo] SET [BuildKey] = '38556544-7317-4c33-aa0e-24a634a4f1f4' WHERE [BuildConfigId] = 13
 	END
 ELSE
 	BEGIN
 		INSERT INTO [dbo].[dbverinfo]
 				( [BuildConfigId], [BuildKey] )
-		VALUES  ( 13, '1cff21d4-c440-4e1f-b075-427ad0d7ff74')
+		VALUES  ( 13, '38556544-7317-4c33-aa0e-24a634a4f1f4')
 	END;
