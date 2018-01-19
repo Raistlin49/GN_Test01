117e85c4b9977c0e13092236a180767a6e821ceb
Raistlin49 <raistlin492008@gmail.com>
2018-01-18 20:15:34 -0500

    MAX CHANGESET af85fed2de35ec3f2973a6c5b83c577e6e0a0cc2

--- Database/Release/SQL/Master/20171024/20171024_master_VDBMP_04_Rollback.sql
+++ Database/Release/SQL/Master/20171024/20171024_master_VDBMP_04_Rollback.sql
@@ -2,16 +2,12 @@
 -- Description: SQL Release Script
 -- Created by:  Data Services
 -- Machine: TGC-LT-94JMM12
--- Created on:  01-18-2018 19:10:19
+-- Created on:  01-18-2018 20:15:23
 -- Path: /Database/Release/SQL/Patch/20171024/VDBMP
 -- Filter: Rollback
 --================================================================================================
 -- Manifest --
 --   DS-1024.01.Alter.Rollback.sql
---   DS-1024.01.Alter.Rollback.sql
---   DS-1024.02.Alter.Rollback.sql
---   DS-1024.02.Alter.Rollback.sql
---   DS-1024.02.Alter.Rollback.sql
 --   DS-1024.02.Alter.Rollback.sql
 -- Manifest --
 
@@ -27,73 +23,11 @@ SET QUOTED_IDENTIFIER ON;
 GO
 PRINT 'Testing code in the rollback script...'
 PRINT 'The 3:22 AM line';
+PRINT 'New line';
+
 ;
 GO
 PRINT 'Processing DS-1024.01.Alter.Rollback.sql ******** End'
-GO
---================================================================================================
--- /Database/Release/SQL/Patch/20171024/VDBMP/DS-1024.01.Alter.Rollback.sql
-PRINT 'Processing DS-1024.01.Alter.Rollback.sql ******** Start'
-GO
-SET ANSI_NULLS ON;
-GO
-SET ANSI_PADDING ON;
-GO
-SET QUOTED_IDENTIFIER ON;
-GO
-PRINT 'Testing code in the rollback script...'
-PRINT 'The 3:22 AM line';
-;
-GO
-PRINT 'Processing DS-1024.01.Alter.Rollback.sql ******** End'
-GO
---================================================================================================
--- /Database/Release/SQL/Patch/20171024/VDBMP/DS-1024.02.Alter.Rollback.sql
-PRINT 'Processing DS-1024.02.Alter.Rollback.sql ******** Start'
-GO
-SET ANSI_NULLS ON;
-GO
-SET ANSI_PADDING ON;
-GO
-SET QUOTED_IDENTIFIER ON;
-GO
-PRINT '02 - Testing code in the rollback script...'
-PRINT '3:24 AM';
-;
-GO
-PRINT 'Processing DS-1024.02.Alter.Rollback.sql ******** End'
-GO
---================================================================================================
--- /Database/Release/SQL/Patch/20171024/VDBMP/DS-1024.02.Alter.Rollback.sql
-PRINT 'Processing DS-1024.02.Alter.Rollback.sql ******** Start'
-GO
-SET ANSI_NULLS ON;
-GO
-SET ANSI_PADDING ON;
-GO
-SET QUOTED_IDENTIFIER ON;
-GO
-PRINT '02 - Testing code in the rollback script...'
-PRINT '3:24 AM';
-;
-GO
-PRINT 'Processing DS-1024.02.Alter.Rollback.sql ******** End'
-GO
---================================================================================================
--- /Database/Release/SQL/Patch/20171024/VDBMP/DS-1024.02.Alter.Rollback.sql
-PRINT 'Processing DS-1024.02.Alter.Rollback.sql ******** Start'
-GO
-SET ANSI_NULLS ON;
-GO
-SET ANSI_PADDING ON;
-GO
-SET QUOTED_IDENTIFIER ON;
-GO
-PRINT '02 - Testing code in the rollback script...'
-PRINT '3:24 AM';
-;
-GO
-PRINT 'Processing DS-1024.02.Alter.Rollback.sql ******** End'
 GO
 --================================================================================================
 -- /Database/Release/SQL/Patch/20171024/VDBMP/DS-1024.02.Alter.Rollback.sql
