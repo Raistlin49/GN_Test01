e6497be15cf1055043689fa24afd9186a17756da
Raistlin49 <raistlin492008@gmail.com>
2018-01-18 21:49:55 -0500

    MAX CHANGESET 51a39da266dea2da90b8c119be1eb651887b40f6

---
+++ Database/Release/SQL/Master/20180107/20180107_master_VDBMP_04_Rollback.sql
@@ -0,0 +1,43 @@
+--================================================================================================
+-- Description: SQL Release Script
+-- Created by:  Data Services
+-- Machine: TGC-LT-94JMM12
+-- Created on:  01-18-2018 21:49:46
+-- Path: /Database/Release/SQL/Patch/20180107/VDBMP
+-- Filter: Rollback
+--================================================================================================
+-- Manifest --
+--   DS-1100.01.Create.Rollback.sql
+-- Manifest --
+
+--================================================================================================
+-- /Database/Release/SQL/Patch/20180107/VDBMP/DS-1100.01.Create.Rollback.sql
+PRINT 'Processing DS-1100.01.Create.Rollback.sql ******** Start'
+GO
+SET ANSI_NULLS ON;
+GO
+SET ANSI_PADDING ON;
+GO
+SET QUOTED_IDENTIFIER ON;
+GO
+USE CRM;
+GO
+
+SELECT 'Happy New Year - Rollback';
+GO
+
+SELECT GETDATE();
+GO
+;
+GO
+PRINT 'Processing DS-1100.01.Create.Rollback.sql ******** End'
+GO
+--================================================================================================
+SET ANSI_NULLS ON;
+GO
+SET ANSI_PADDING ON;
+GO
+SET QUOTED_IDENTIFIER ON;
+GO
+--================================================================================================
+-- End of Script
