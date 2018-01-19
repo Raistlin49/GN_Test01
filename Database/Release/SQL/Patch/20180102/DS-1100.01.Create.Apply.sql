62b122753b065a4278d4feb281a6e4e9b2d4e7a6
Raistlin49 <raistlin492008@gmail.com>
2018-01-18 21:28:33 -0500

    New Sprint - 20180102
    [JIRA=DS-3174]

---
+++ Database/Release/SQL/Patch/20180102/DS-1100.01.Create.Apply.sql
@@ -0,0 +1,8 @@
+USE TableBackup;
+GO
+
+SELECT 'Happy New Year - Apply';
+GO
+
+SELECT GETDATE();
+GO
---
+++ Database/Release/SQL/Patch/20180102/DS-1100.01.Create.Rollback.sql
@@ -0,0 +1,8 @@
+USE TableBackup;
+GO
+
+SELECT 'Happy New Year - Rollback';
+GO
+
+SELECT GETDATE();
+GO
