fa1a012a6e6fae7493b4861ede43b611473b28ea
Raistlin49 <raistlin492008@gmail.com>
2018-01-20 03:50:22 -0500

    MAX CHANGESET fc1378b7625553006b378635b55803759c50ffa8

--- Database/Release/SQL/Master/20180107/20180107_master_VDBMP_04_Rollback.sql
+++ Database/Release/SQL/Master/20180107/20180107_master_VDBMP_04_Rollback.sql
@@ -1,14 +1,14 @@
---================================================================================================
--- Description: SQL Release Script
--- Created by:  Data Services
--- Machine: TGC-LT-94JMM12
--- Created on:  01-19-2018 16:43:17
--- Path: /Database/Release/SQL/Patch/20180107/VDBMP
--- Filter: Rollback
---================================================================================================
--- Manifest --
+--=====HEADER=====================================================================================
+-- Description:	SQL Release Script;
+-- Created by: Data Services;
+-- Machine: TGC-LT-94JMM12;
+-- Created on: 2018-01-20 03: 50:15
+-- Path: /Database/Release/SQL/Master/20180107/20180107_master_VDBMP_04_Rollback.sql
+-- Sprint: 20180107 VDBMP
+-- Filter: Rollback;
+--=====MANIFEST===================================================================================
 --   DS-1100.01.Create.Rollback.sql
--- Manifest --
+--=====PATCHES====================================================================================
 
 --================================================================================================
 -- /Database/Release/SQL/Patch/20180107/VDBMP/DS-1100.01.Create.Rollback.sql
@@ -32,12 +32,12 @@ GO
 GO
 PRINT 'Processing DS-1100.01.Create.Rollback.sql ******** End'
 GO
+--=====FOOTER=====================================================================================
+SET ANSI_NULLS ON;;
+GO;
+SET ANSI_PADDING ON;;
+GO;
+SET QUOTED_IDENTIFIER ON;;
+GO;
 --================================================================================================
-SET ANSI_NULLS ON;
-GO
-SET ANSI_PADDING ON;
-GO
-SET QUOTED_IDENTIFIER ON;
-GO
---================================================================================================
--- End of Script
+-- End of Script;
