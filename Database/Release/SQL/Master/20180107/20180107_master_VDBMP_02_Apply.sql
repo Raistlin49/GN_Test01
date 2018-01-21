367510b13023dff28d47d1eec7f6e98a494c51d1
Raistlin49 <raistlin492008@gmail.com>
2018-01-20 20:25:33 -0500

    MAX CHANGESET 27b19e7aa5312cdca3d6779aba31db793cb064d1

--- Database/Release/SQL/Master/20180107/20180107_master_VDBMP_02_Apply.sql
+++ Database/Release/SQL/Master/20180107/20180107_master_VDBMP_02_Apply.sql
@@ -20,26 +20,3 @@ BEGIN
 END
 GO
 --*************************************--
-
-USE [CRM]
-GO
-
---Create missing table
-IF OBJECT_ID('[dbo].[dbverinfo]', 'U') IS NULL
-BEGIN
-	CREATE TABLE [dbo].[dbverinfo](
-		[BuildConfigId] [INT] NOT NULL,
-		[BuildKey] [VARCHAR](100) NOT NULL
-	) ON [PRIMARY]
-END;
-
-IF EXISTS (SELECT 1 FROM [dbo].[dbverinfo] WHERE [BuildConfigId] = 13)
-	BEGIN
-		UPDATE [dbo].[dbverinfo] SET [BuildKey] = '5241e1cb-080b-4e99-961b-c980351ef68e' WHERE [BuildConfigId] = 13
-	END
-ELSE
-	BEGIN
-		INSERT INTO [dbo].[dbverinfo]
-				( [BuildConfigId], [BuildKey] )
-		VALUES  ( 13, '5241e1cb-080b-4e99-961b-c980351ef68e')
-	END;
