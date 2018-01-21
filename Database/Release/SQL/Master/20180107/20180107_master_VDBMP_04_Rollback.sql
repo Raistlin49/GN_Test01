be63c4005430a4f4bce2062f156de318f5e07fc7
Raistlin49 <raistlin492008@gmail.com>
2018-01-20 20:15:08 -0500

    MAX CHANGESET 663b3f04be9e6f39fc7e4fc5f9cf7233cc544550

--- Database/Release/SQL/Master/20180107/20180107_master_VDBMP_04_Rollback.sql
+++ Database/Release/SQL/Master/20180107/20180107_master_VDBMP_04_Rollback.sql
@@ -1,43 +1,22 @@
---=====HEADER=====================================================================================
--- Description:	SQL Release Script;
--- Created by: Data Services;
--- Machine: TGC-LT-94JMM12;
--- Created on: 2018-01-20 03: 50:15
--- Path: /Database/Release/SQL/Master/20180107/20180107_master_VDBMP_04_Rollback.sql
--- Sprint: 20180107 VDBMP
--- Filter: Rollback;
---=====MANIFEST===================================================================================
---   DS-1100.01.Create.Rollback.sql
---=====PATCHES====================================================================================
-
---================================================================================================
--- /Database/Release/SQL/Patch/20180107/VDBMP/DS-1100.01.Create.Rollback.sql
-PRINT 'Processing DS-1100.01.Create.Rollback.sql ******** Start'
-GO
-SET ANSI_NULLS ON;
-GO
-SET ANSI_PADDING ON;
-GO
-SET QUOTED_IDENTIFIER ON;
-GO
 USE CRM;
 GO
 
-SELECT 'Happy New Year - Rollback';
+--*****UPGRADE FROM v2 TO v3******--
+--Add MasterType column
+IF NOT EXISTS (SELECT * FROM sys.columns WHERE object_id = OBJECT_ID('dbverinfo', 'U') AND name = 'MasterType' )
+BEGIN
+
+	ALTER TABLE [dbverinfo] ADD [MasterType] VARCHAR(100) NULL;
+
+END
 GO
 
-SELECT GETDATE();
+--Add EventDate column
+IF NOT EXISTS (SELECT * FROM sys.columns WHERE object_id = OBJECT_ID('dbverinfo', 'U') AND name = 'EventDate' )
+BEGIN
+
+	ALTER TABLE [dbverinfo] ADD [EventDate] DATETIME NULL;
+
+END
 GO
-;
-GO
-PRINT 'Processing DS-1100.01.Create.Rollback.sql ******** End'
-GO
---=====FOOTER=====================================================================================
-SET ANSI_NULLS ON;;
-GO;
-SET ANSI_PADDING ON;;
-GO;
-SET QUOTED_IDENTIFIER ON;;
-GO;
---================================================================================================
--- End of Script;
+--*************************************--
