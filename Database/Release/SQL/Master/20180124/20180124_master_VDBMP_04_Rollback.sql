0d3b06f6b63340cd80b96f5e04911c88134d9438
Raistlin49 <raistlin492008@gmail.com>
2018-01-22 19:17:54 -0500

    MAX CHANGESET e84c5c6851466c623b6e6eaaee3b9c0e0db34c3f

---
+++ Database/Release/SQL/Master/20180124/20180124_master_VDBMP_04_Rollback.sql
@@ -0,0 +1,104 @@
+--=====HEADER=====================================================================================
+-- Description:	SQL Release Script
+-- Created by: Data Services
+-- Machine: TGC-LT-94JMM12
+-- Created on: 2018-01-22 19: 17:47
+-- Path: /Database/Release/SQL/Master/20180124/20180124_master_VDBMP_04_Rollback.sql
+-- Sprint: 20180124 VDBMP
+-- Filter: Rollback
+
+SET DEADLOCK_PRIORITY HIGH;
+GO
+
+--=====MANIFEST===================================================================================
+--   DS-5555.01.Create.Rollback.sql
+
+--=====PATCHES====================================================================================
+
+--================================================================================================
+-- /Database/Release/SQL/Patch/20180124/VDBMP/DS-5555.01.Create.Rollback.sql
+PRINT 'Processing DS-5555.01.Create.Rollback.sql ******** Start'
+GO
+SET ANSI_NULLS ON;
+GO
+SET ANSI_PADDING ON;
+GO
+SET QUOTED_IDENTIFIER ON;
+GO
+PRINT 'New rollback for 20180124';
+GO
+;
+GO
+PRINT 'Processing DS-5555.01.Create.Rollback.sql ******** End'
+GO
+
+--=====DBVERINFO==================================================================================
+USE CRM;
+GO
+
+--*****UPGRADE FROM v2 TO v3******--
+
+--Add Note column
+IF NOT EXISTS (SELECT * FROM sys.columns WHERE object_id = OBJECT_ID('dbverinfo', 'U') AND name = 'EventNote' )
+BEGIN
+
+	ALTER TABLE [dbverinfo] ADD [EventNote] VARCHAR(1000) NULL;
+
+END
+GO
+
+--Add MasterType column
+IF NOT EXISTS (SELECT * FROM sys.columns WHERE object_id = OBJECT_ID('dbverinfo', 'U') AND name = 'MasterType' )
+BEGIN
+
+	ALTER TABLE [dbverinfo] ADD [MasterType] VARCHAR(100) NULL;
+
+END
+GO
+
+--Add EventDate column
+IF NOT EXISTS (SELECT * FROM sys.columns WHERE object_id = OBJECT_ID('dbverinfo', 'U') AND name = 'EventDate' )
+BEGIN
+
+	ALTER TABLE [dbverinfo] ADD [EventDate] DATETIME NULL;
+
+	EXEC ('UPDATE [dbverinfo] SET [EventDate] = DATEADD(D, -1, GETDATE())');
+
+END
+GO
+
+--*************************************--
+USE CRM;
+GO
+
+--Add event
+INSERT INTO dbo.dbverinfo (BuildConfigId, BuildKey, EventNote, MasterType, EventDate)
+	VALUES (15, '4db796fb-1b8a-4463-8907-e0779253eb6f', '20180124 VDBMP', 'Rollback', GETDATE());
+GO
+
+--=====FOOTER=====================================================================================
+SET ANSI_NULLS ON;
+GO
+SET ANSI_PADDING ON;
+GO
+SET QUOTED_IDENTIFIER ON;
+GO
+--================================================================================================
+-- End of Script;
+
+--=====HELPTEXT===================================================================================
+--<{pathparse subexpr}>-- e.g. <sprintdate>-- e.g. --<instance>--
+--<verinfodb>--             --db containing dbverinfo table
+--<buildkey>--              --CI GUID per master changeset
+--<repomasterpath>--        --Path in repo to folder containing this master
+--<buildconfigid>--         --
+--<buildid>--
+--<changeset>--
+--<mastertype>--
+--<templateid:{ci.templates.templateid}>--
+--<patches>--
+--<manifest>--
+--<ciservername>--
+--<builddate>--
+--<helptext>--
+--<buildconfigname>--
